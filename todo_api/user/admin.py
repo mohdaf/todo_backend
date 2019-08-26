2834acc8cde54944c362317519a2e0fad3c0d068
Mohammed <mabdlmajid@gmail.com>
2019-08-26 18:29:15 +0400

    added user admin config

--- todo_api/user/admin.py
+++ todo_api/user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from . import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['username', 'country']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Location Info'), {'fields': ('country','city')})
+        )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.TodoUser, UserAdmin)
