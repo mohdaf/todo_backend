ee4b9ee1b3d00b48204054f3293d2b6980df2f72
Mohammed <mabdlmajid@gmail.com>
2019-08-26 19:12:30 +0400

    added todo model and admin config

--- todo_api/todo/models.py
+++ todo_api/todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Todo(models.Model):
+    """Todo object"""
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_on = models.DateTimeField('Created on')
+
+    def __str__(self):
+        return self.title
