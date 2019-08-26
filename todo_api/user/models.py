c34a985b47b4426f138c9b2963b4cb39ad46bb51
Mohammed <mabdlmajid@gmail.com>
2019-08-26 16:17:56 +0400

    added user creation endpoint

--- todo_api/user/models.py
+++ todo_api/user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class TodoUser(AbstractUser):
+    # add additional fields in here
+
+    country = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return self.username
